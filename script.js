e616f0045123c2a993319126797ecc65c08a635f
NAHammell <nicholas.a.hammell@gmail.com>
2023-02-14 22:05:34 -0500

    Add play round function

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     let index = Math.floor(Math.random() * choices.length);
 
     return choices[index];
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors') {
+        return 'Invalid input';
+    }
+
+    if (playerChoice.toLowerCase() === computerChoice) {
+        return 'You tied.';
+    } else if ((playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors') || (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        return 'You win!';
+    } else {
+        return 'You lose...'
+    }
 }
