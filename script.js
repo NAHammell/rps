394571abd27f249a6a6480897017e91f84e1bc7a
NAHammell <nicholas.a.hammell@gmail.com>
2023-02-14 22:16:30 -0500

    Add scoring system

--- script.js
+++ script.js
@@ -11,16 +11,33 @@ function playRound(playerChoice, computerChoice) {
     }
 
     if (playerChoice.toLowerCase() === computerChoice) {
-        return 'You tied.';
+        return 0;
     } else if ((playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissors') || (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        return 'You win!';
+        return 1;
     } else {
-        return 'You lose...'
+        return -1;
     }
 }
 
 function playGame() {
+    let score = 0;
+
     for (let i = 0; i < 5; i ++) {
-        console.log(playRound(prompt(), getComputerChoice()));
+        let outcome = playRound(prompt(), getComputerChoice());
+        if (outcome === 1) {
+            score++;
+            console.log('You won!');
+        } else if (outcome === -1) {
+            score--;
+            console.log('You lost...');
+        } else {
+            console.log('You tied.');
+        }
+    }
+
+    if (score > 1) {
+        return 'You won the game!';
+    } else {
+        return 'You lost the game...';
     }
 }
