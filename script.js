519e87551f0e0dbcbd129b947a2e48cf876f434d
NAHammell <nicholas.a.hammell@gmail.com>
2023-02-21 11:02:17 -0500

    Add CPU highlighting and refactor game logic

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const playerRock = document.querySelector('#rock');
 const playerPaper = document.querySelector('#paper');
 const playerScissors = document.querySelector('#scissors');
 
+const cpuRock = document.querySelector('#cpu-rock');
+const cpuPaper = document.querySelector('#cpu-paper');
+const cpuScissors = document.querySelector('#cpu-scissors');
+
 const playerScore = document.querySelector('#player-score');
 const cpuScore = document.querySelector('#cpu-score');
 
@@ -16,44 +20,48 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    playerChoice = this.id;
-    computerChoice = getComputerChoice();
-
-    removeUserHighlight();
-    this.classList.add('chosen');
-
-    if (playerChoice === computerChoice) {
-
-    } else if (playerChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            cpuWins += 1;
-            cpuScore.textContent = cpuWins;
-        } else {
-            playerWins += 1;
-            playerScore.textContent = playerWins;
+    if (playerWins < 5 && cpuWins < 5) {
+        playerChoice = this.id;
+        computerChoice = getComputerChoice();
+    
+        removeUserHighlight();
+        removeCpuHighlight();
+        cpuHighlight(computerChoice);
+        this.classList.add('chosen');
+    
+        if (playerChoice === computerChoice) {
+    
+        } else if (playerChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                cpuWins += 1;
+                cpuScore.textContent = cpuWins;
+            } else {
+                playerWins += 1;
+                playerScore.textContent = playerWins;
+            }
+        } else if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                cpuWins += 1;
+                cpuScore.textContent = cpuWins;
+            } else {
+                playerWins += 1;
+                playerScore.textContent = playerWins;
+            }
+        } else if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                cpuWins += 1;
+                cpuScore.textContent = cpuWins;
+            } else {
+                playerWins += 1;
+                playerScore.textContent = playerWins;
+            }
         }
-    } else if (playerChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            cpuWins += 1;
-            cpuScore.textContent = cpuWins;
-        } else {
-            playerWins += 1;
-            playerScore.textContent = playerWins;
+    
+        if (playerWins === 5) {
+            result.textContent = 'You have 5 points, you win!';
+        } else if (cpuWins === 5) {
+            result.textContent = 'The CPU has 5 points, you lose...';
         }
-    } else if (playerChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-            cpuWins += 1;
-            cpuScore.textContent = cpuWins;
-        } else {
-            playerWins += 1;
-            playerScore.textContent = playerWins;
-        }
-    }
-
-    if (playerWins === 5) {
-        result.textContent = 'You have 5 points, you win!';
-    } else if (cpuWins === 5) {
-        result.textContent = 'The CPU has 5 points, you lose...';
     }
 }
 
@@ -61,6 +69,17 @@ function removeUserHighlight() {
     playerRock.classList.remove('chosen');
     playerPaper.classList.remove('chosen');
     playerScissors.classList.remove('chosen');
+}
+
+function removeCpuHighlight() {
+    cpuRock.classList.remove('cpu-chosen');
+    cpuPaper.classList.remove('cpu-chosen');
+    cpuScissors.classList.remove('cpu-chosen');
+}
+
+function cpuHighlight(cpuButton) {
+    const cpuChoice = document.querySelector(`#cpu-${cpuButton}`);
+    cpuChoice.classList.add('cpu-chosen');
 }
 
 playerRock.addEventListener('click', playRound);
